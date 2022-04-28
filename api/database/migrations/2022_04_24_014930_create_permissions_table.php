46e681c439128f6a5b39b0daef324c0fe4ef20e6
duruo <duruo@126.com>
2022-04-28 08:14:28 +0800

    rbac-table

--- api/database/migrations/2022_04_24_014930_create_permissions_table.php
+++ api/database/migrations/2022_04_24_014930_create_permissions_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedTinyInteger('type');
-            $table->string('name', 64);
-            $table->text('description')->nullable();
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
 
             $table->index('type');
